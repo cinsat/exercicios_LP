ff0a5ad5b3641f6d10dda65a4212b46172acd4bf
unknown <cintiasatomi@hotmail.com>
2022-06-26 12:48:02 -0300

    exercicio 1 - LP

---
+++ aula_24.05.js
@@ -0,0 +1,35 @@
+/* Exercício Prático 1
+
+Crie variáveis com os tipos de dados que você aprendeu na auala de hoje:
+1. String
+2. Int
+3. Double
+4. Float 
+5. Boolean
+
+PS: a diferença entre Float e Double é a capacidade de armazenamento de cada um, sendo Float até 7 casas decimais depois da virgula e o Double até 15 casas decimais depois da vírgula
+
+Em seguida concatene elas para formar uma frase e mostre no console
+*/ 
+
+/*Variáveis sobre pets:
+
+1. Nomes dos pets?
+2. Quantidade de pets?
+3. Valor da ração?
+4. Peso dos pets?
+5. São somente cachorros?
+*/
+
+const nomePets = "Polly, Tato e Katrina";
+
+let quantidadePets = 3;
+
+let idadePets = "8, 10 e 6";
+
+let racaPets = "vira-lata, Shitzu e York";
+
+let saoAdotados = true;
+let saoComprados = false;
+
+console.log(`Eu tenho ${quantidadePets} pets, com os nomes de ${nomePets} e com ${idadePets} anos respectivamente. As raças são ${racaPets} e são adotados ${saoAdotados}`);
